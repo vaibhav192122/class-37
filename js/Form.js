81b0905f15a0d8d705534daed81f2eb50d5d8ed8
vaibhav192122 <73110783+vaibhav192122@users.noreply.github.com>
2021-01-19 15:03:51 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,42 @@
+class Form {
+  constructor() {
+    this.title = createElement('h2');
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h3');
+  }
+
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    
+    this.title.html("Car Racing Game");
+    this.title.position(130, 0);
+    
+    
+    
+    
+    this.input.position(130, 160);
+    this.button.position(250, 200);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this. button.hide();
+
+      player.name = this.input.value();
+      
+      playerCount+=1;
+      player.update()
+      player.index = playerCount;
+      player.updateCount(playerCount);
+      
+      this.greeting.html("Hello " + player.name )
+      this.greeting.position(130, 160)
+    });
+
+  }
+}
